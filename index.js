1166666ecd108532685ec86c40fa894b8b422e4b
Kyle Mitchell <kyle@kemitchell.com>
2015-08-20 17:37:20 -0700

    Apply commonform-archaic only once

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var predicate = require('commonform-predicate')
 var archaic = require('commonform-archaic')
+var predicate = require('commonform-predicate')
 
 var rules = [
   require('./rules/phrases'),
@@ -7,35 +7,32 @@ var rules = [
 
 var recurse = function(form, path, annotations) {
   return annotations
-
     // Annotations about `form`
     .concat(
       rules.reduce(
         function(annotations, rule) {
           return annotations.concat(rule(form, path)) },
-        []))
-
-    .concat(archaic(form, path))
-
+        [ ]))
     // Annotations about children of `form`.
     .concat(
       form.content.reduce(
         function(annotations, element, index) {
           if (predicate.child(element)) {
             var childForm = element.form
-            var childPath = path.concat(['content', index, 'form'])
+            var childPath = path.concat([ 'content', index, 'form' ])
             return annotations.concat(
-              recurse(childForm, childPath, [])) }
+              recurse(childForm, childPath, [ ])) }
           else {
             return annotations } },
-        [])) }
+        [ ])) }
 
 module.exports = function(form) {
-  return recurse(form, [], [])
-    .map(function(annotation) {
-      if (!annotation.hasOwnProperty('source')) {
-        annotation.source = 'commonform-critique' }
-      /* istanbul ignore else */
-      if (!annotation.hasOwnProperty('url')) {
-        annotation.url = null }
-      return annotation }) }
+  return archaic(form)
+    .concat(
+      recurse(form, [ ], [ ])
+        .map(function(annotation) {
+          if (!annotation.hasOwnProperty('source')) {
+            annotation.source = 'commonform-critique' }
+          if (!annotation.hasOwnProperty('url')) {
+            annotation.url = null }
+          return annotation })) }
